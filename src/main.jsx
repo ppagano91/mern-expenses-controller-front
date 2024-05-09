9661db7b7c6240d274c08a07d7454bb298149029
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-09 17:07:00 -0300

    Configuración de  React Query

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { store } from './redux/store/store.js'
 
 import './index.css'
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>      
-      <App />
+    <Provider store={store}>
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
