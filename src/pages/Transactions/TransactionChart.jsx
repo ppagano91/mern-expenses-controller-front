abd9a134a9e22552664595261dcbe8d4940ad753
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-10 14:44:43 -0300

    Agrego diagrama circular para visualizar ingresos vs gastos

--- src/pages/Transactions/TransactionChart.jsx
+++ src/pages/Transactions/TransactionChart.jsx
@@ -29,12 +29,58 @@ const TransactionChart = () => {
     };
     return accumlator;
   },{income: 0, expense: 0});
+
+  const data = {
+    labels: ["Income", "Expense"],
+    datasets: [
+      {
+        label: "Transactions",
+        data: [totals?.income, totals?.expense],
+        backgroundColor: ["#36A2EB", "#FF6384"],
+        borderColor: ["#36A2EB", "#FF6384"],
+        borderWith: 1,
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          padding: 25,
+          boxWidth: 12,
+          font: {
+            size: 14,
+          },
+        },
+      },
+      title: {
+        display: true,
+        text: "Income vs Expense",
+        font: {
+          size: 18,
+          weight: "bold",
+        },
+        padding: {
+          top: 10,
+          bottom: 30,
+        },
+      },
+    },
+    cutout: "70%",
+  };
+
   return (
     <div className="my-8 p-6 bg-white rounded-lg shadow-xl border border-gray-200">
       <h1 className="text-2xl font-bold text-center mb-6">
         Transaction Overview
       </h1>
-      <div style={{ height: "350px" }}>{/* <Doughnut  /> */}</div>
+      <div style={{ height: "350px" }}>
+        <Doughnut data={data} options={options} />
+      </div>
     </div>
   );
 };
